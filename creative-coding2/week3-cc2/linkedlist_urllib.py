27acd144c968143a246a7b06fe472e5c495645ae
Filippo <romeofilippo95@gmail.com>
2021-02-07 11:47:36 +0100

    Challage 4

---
+++ creative-coding2/week3-cc2/linkedlist_urllib.py
@@ -0,0 +1,85 @@
+import shutil
+import tempfile
+import requests
+import re
+
+url_py_challange = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={nothing}'
+
+nothing = '16044'
+
+def iterationtrulink(x):
+    req = requests.get(url_py_challange, params= {"nothing": x})
+
+    match = re.findall(r"([0-9])", req.text)
+    if match:
+        nextNothing = (''.join(str(x) for x in match))
+        
+        print("               ")
+        print("               ")
+        print("The current nothing is " + x)
+        print(req.text)
+        #print("nextNothing " + nextNothing)
+        print("               ")
+        print("               ")
+        
+
+        x = nextNothing
+        return iterationtrulink(x)
+    
+    else:
+        print("\n ALT\n")
+        nextNothing = (''.join(str(x) for x in match))
+        
+        print (nextNothing)
+        print(req.text)
+        print(x)
+        
+        print("               ")
+        print("               ")
+        print("               ")
+
+        print("***************")
+        print("***************")
+        print("               ")
+        print("               ")
+
+        print("Do you want to: ")
+        print("               ")
+        print("Type 1 --> Divide by two and continue  ")
+        print("Type any char --> Stop")
+
+        print("               ")
+        print("               ")
+        print("***************")
+        print("***************")
+
+        print("               ")
+        val = input("Type in, come on we got requestss to make!: ")
+        print("               ")
+
+        def zero (x):
+            
+            x = int(x)
+            x = x/2
+            x = int(x)
+            x = str(x)
+            print(x)
+            return iterationtrulink(x)
+
+        if val == "1":
+            zero(x)
+        else:
+            print("Ok")
+            i = 0
+            while i < 10 :
+
+                print("***************")
+                print("Ok that was it.")
+                i += 1
+
+            
+
+        
+        
+
+iterationtrulink(nothing)
--- creative-coding2/week3-cc2/urllib.py
+++
@@ -1,9 +0,0 @@
-import shutil
-import tempfile
-import requests
-
-x = requests.get('http://www.pythonchallenge.com/pc/def/linkedlist.php')
-
-print(x)
-
-http://www.pythonchallenge.com/pc/def/linkedlist.php
