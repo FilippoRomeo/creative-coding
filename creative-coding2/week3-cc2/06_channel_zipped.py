425b6a7f4e44fe2edfbc128cadbf89c677aec1c8
Filippo <romeofilippo95@gmail.com>
2021-02-09 13:19:25 +0100

    challenge6

---
+++ creative-coding2/week3-cc2/06_channel_zipped.py
@@ -0,0 +1,46 @@
+import re, os, zipfile
+
+nothing = 90052
+comments = []
+
+def iterationtrulink(x, y):
+
+    cur_path = os.path.dirname(__file__)
+    folder = "channel.zip"
+    pathtofolder = os.path.join(cur_path, folder)
+
+    file_extns = '{}.txt'
+
+    f = zipfile.ZipFile(pathtofolder, "r")
+
+    content = f.read(file_extns.format(x))
+    y.append(f.getinfo(file_extns.format(x)).comment.decode('utf-8'))
+
+    content = str(content)
+    match = re.findall(r"([0-9])", content)    
+    match = "".join(match)
+
+    if match:
+               
+        print("               ")
+        print("               ")
+        x = str(x)
+        print("The current nothing is " + x)
+        print(comments)
+        print(match)
+        print(''.join(y))
+        print("               ")
+        print("               ")
+        
+        return iterationtrulink(match, y)
+    else:
+        print("\n ALT\n")
+        nextNothing = (''.join(str(x) for x in match))
+        
+        print(nextNothing)
+        print(comments)
+        print(match)
+        print(x)
+        print(''.join(y))
+ 
+iterationtrulink(nothing, comments)
